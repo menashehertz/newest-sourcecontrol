220642aadaeb5e860a841a48c9d96b3213fdad97
Steven Hertz <hertzsteven@icloud.com>
2018-08-13 16:04:24 -0400

    Initial Commit

---
+++ test source control/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  test source control
+//
+//  Created by Steven Hertz on 8/13/18.
+//  Copyright © 2018 DevelopItSolutions. All rights reserved.
+//
+
+import UIKit
+
+var pointInTime = ""
+
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        pointInTime = "before Version control"
+        
+        
+    }
+
+
+
+}
+
